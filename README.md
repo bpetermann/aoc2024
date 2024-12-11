cdef48b13dab9b9e854cdbce68f6aa3106a7cdab
bpetermann <benjamin.petermann@gmx.at>
2024-12-11 11:06:35 +0100

    feat: add day 08

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -111,16 +111,16 @@ Both parts: 2484.092ms
 
 ```
 Day 07
-Time part 1: 90.373ms
-Time part 2: 5478.089ms
-Both parts: 5568.462ms
+Time part 1: 89.224ms
+Time part 2: 5173.045ms
+Both parts: 5262.268ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 6.71ms
+Time part 2: 7.371ms
+Both parts: 14.082ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 8122.335ms
+Total stars: 16/50
+Total time: 7830.224ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day08/index.ts
@@ -0,0 +1,120 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((row) => row.split(""));
+
+const getSymbols = (
+  input: string[][],
+  x: number,
+  y: number,
+  symbol: string,
+) => {
+  const locations: [number, number][] = [];
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      if (!(i === x && j === y) && input[i][j] === symbol) {
+        locations.push([i, j]);
+      }
+    }
+  }
+
+  return locations;
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const nodes = new Set<string>();
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      if (input[i][j] !== ".") {
+        const possibleNodes = getSymbols(input, i, j, input[i][j]);
+
+        possibleNodes.forEach(([x, y]) => {
+          let nextX = 2 * i - x;
+          let nextY = 2 * j - y;
+
+          if (
+            nextX >= 0 &&
+            nextX < input.length &&
+            nextY >= 0 &&
+            nextY < input[0].length
+          ) {
+            nodes.add(`${[nextX, nextY]}`);
+          }
+        });
+      }
+    }
+  }
+
+  return nodes.size;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const nodes = new Set<string>();
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      if (input[i][j] !== ".") {
+        const possibleNodes = getSymbols(input, i, j, input[i][j]);
+
+        possibleNodes.forEach(([x, y]) => {
+          let currentX = i;
+          let currentY = j;
+
+          let nextX = 2 * i - x;
+          let nextY = 2 * j - y;
+
+          nodes.add(`${x},${y}`);
+
+          while (
+            nextX >= 0 &&
+            nextX < input.length &&
+            nextY >= 0 &&
+            nextY < input[0].length
+          ) {
+            nodes.add(`${nextX},${nextY}`);
+
+            const prevX = currentX;
+            const prevY = currentY;
+
+            currentX = nextX;
+            currentY = nextY;
+
+            nextX = 2 * nextX - prevX;
+            nextY = 2 * nextY - prevY;
+          }
+        });
+      }
+    }
+  }
+
+  return nodes.size;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
