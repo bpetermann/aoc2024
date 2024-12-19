f5904ee96be2b3e704f7df783747bdf1e119294e
bpetermann <benjamin.petermann@gmx.at>
2024-12-19 09:21:59 +0100

    feat: add day 14 part 2

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,29 +1,35 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((m) => m.match(/-?\d+/g)?.map(Number)) as number[][];
+
+function endPositions(
+  [startX, startY, x, y]: number[],
+  count: number,
+  width: number,
+  height: number,
+) {
+  let endX = (startX + count * x) % width;
+  let endY = (startY + count * y) % height;
+
+  if (endY < 0) endY += height;
+  if (endX < 0) endX += width;
+
+  return [endX, endY];
+}
+
+const width = 101;
+const height = 103;
+
+const halfWidth = Math.floor(width / 2);
+const halfHeight = Math.floor(height / 2);
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput)
-    .split("\n")
-    .map((m) => m.match(/-?\d+/g)?.map(Number)) as number[][];
+  const input = parseInput(rawInput);
 
-  const width = 101;
-  const height = 103;
-
-  const halfWidth = Math.floor(width / 2);
-  const halfHeight = Math.floor(height / 2);
-
-  function calculateEndPosition([startX, startY, x, y]: number[]) {
-    let endX = (startX + 100 * x) % width;
-    let endY = (startY + 100 * y) % height;
-
-    if (endY < 0) endY += height;
-    if (endX < 0) endX += width;
-
-    return [endX, endY];
-  }
-
-  const finalPositions = input.map((value) => calculateEndPosition(value));
+  const finalPositions = input.map((value) =>
+    endPositions(value, 100, width, height),
+  );
 
   let final: string[][] = [];
 
@@ -64,7 +70,38 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  let count = 0;
+
+  function isTree(array: number[][]) {
+    let isTree = false;
+
+    array.forEach(([x1, y1], _i, self) => {
+      if (
+        self.find(([x2, y2]) => x2 === x1 - 1 && y1 + 1 === y2) &&
+        self.find(([x2, y2]) => x2 === x1 + 1 && y1 + 1 === y2) &&
+        self.find(([x2, y2]) => x2 === x1 - 2 && y1 + 2 === y2) &&
+        self.find(([x2, y2]) => x2 === x1 + 2 && y1 + 2 === y2) &&
+        self.find(([x2, y2]) => x2 === x1 - 3 && y1 + 3 === y2) &&
+        self.find(([x2, y2]) => x2 === x1 + 3 && y1 + 3 === y2)
+      ) {
+        isTree = true;
+      }
+    });
+
+    return isTree;
+  }
+
+  while (true) {
+    const finalPositions = input.map((value) =>
+      endPositions(value, count, width, height),
+    );
+
+    if (isTree(finalPositions)) break;
+
+    count++;
+  }
+
+  return count;
 };
 
 run({
